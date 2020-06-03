7376d3f89c2c26bb137272a70d41c5afb4f905ac
xiaoyao <635796359@qq.com>
2020-06-03 14:31:22 +0800

    增加加密和解密文件返回buffer

--- test/test.js
+++ test/test.js
@@ -1,12 +1,33 @@
 const DES = require('../index');
-// 字符串加解密
-// const result = AES.EncryptText("_+", "ArcVideo")
-// console.log("*-*-*",result)
+const fs = require("fs");
+(async ()=>{
+  // 字符串加密
+  // const result = AES.EncryptText("_+", "ArcVideo")
+  // console.log("*-*-*",result)
 
-// const result1 = AES.DecryptText(result, "ArcVideo")
-// console.log("*-*-*["+result1+"]")
+  // 字符串加解密
+  // const result1 = AES.DecryptText(result, "ArcVideo")
+  // console.log("*-*-*["+result1+"]")
 
-//文件加解密
-// DES.EncryptFile("./plain.txt", "./plain.txt_aes", "test_password");
- 
-DES.DecryptFile("./plain.txt_aes", "./plain_1.txt", "test_password")
+  // 获取文件加密的二进制数据
+  // console.time("加密文件返回buffer")
+  // const pwdBuffer = await DES.EncryptFileBuffer("./plain.txt", "test_password");
+  // if(pwdBuffer.length){
+  //   fs.writeFileSync("buffer.txt_aes", pwdBuffer)
+  // }
+  // console.timeEnd("加密文件返回buffer")
+
+  // 获取文件解密的二进制数据
+  // console.time("解密文件返回buffer")
+  // const plainBuffer = await DES.DecryptFileBuffer("./buffer.txt_aes", "test_password")
+  // if(plainBuffer.length){
+  //   fs.writeFileSync("buffer.txt", plainBuffer)
+  // }
+  // console.timeEnd("解密文件返回buffer")
+
+  // 加密文件
+  // await DES.EncryptFile("./plain.txt", "./plain.txt_aes", "test_password");
+
+  //解密文件
+  await DES.DecryptFile("./plain.txt_aes", "./plain_1.txt", "test_password")
+})()
